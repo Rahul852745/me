0762ff1e149c84ad027e8940c8cefbcdd54b928d
Rahul852745 <mauryarahul852@gmail.com>
2021-06-07 14:46:07 +0530

    Updates

---
+++ precache-manifest.b02a5e6b57dcd8d336c7d11d5770c07c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3987f316482965fbe25",
+    "url": "/me/static/css/main.e7c31364.chunk.css"
+  },
+  {
+    "revision": "b3987f316482965fbe25",
+    "url": "/me/static/js/main.b3987f31.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "883343d6a9e8151bcc40",
+    "url": "/me/static/js/2.883343d6.chunk.js"
+  },
+  {
+    "revision": "e87fce04b75eb7f0c004418340a9dfbf",
+    "url": "/me/index.html"
+  }
+];
